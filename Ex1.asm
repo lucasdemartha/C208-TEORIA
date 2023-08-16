bcfa474ec293cceed0fe6bf08ec512f13dc449b3
lucasdemartha <lucasribm@gmail.com>
2023-08-15 22:51:15 -0300

    att

---
+++ Ex1.asm
@@ -0,0 +1,7 @@
+.data
+nome: .asciiz "Jonas"
+escola: .ascii "Inatel"
+idade: .byte 0x20
+num: .byte 0x10
+num2: .half 0x6070
+matricula: .word 0x50
---
+++ Ex2.asm
@@ -0,0 +1,6 @@
+.data 0x10010008
+
+nome: .asciiz "Chico"
+idade: .byte 0x30
+cep: .word 0x102030
+matricula: .half 0x5060
